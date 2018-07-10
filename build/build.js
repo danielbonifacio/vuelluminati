bbdb9e28c532b386e1855845dd93093791f3530d
Daniel Bonifacio <danielbonifacio@outlook.com>
2018-07-10 18:14:34 -0300

    dsajdiaufha

--- build/build.js
+++ build/build.js
@@ -1,5 +1,9 @@
 'use strict'
 require('./check-versions')()
+
+// Antes de tudo, define o ambiente como Produção
+process.env.NODE_ENV = 'production'
+
 const fs = require('fs')
 const ora = require('ora')
 const rm = require('rimraf')
@@ -9,66 +13,69 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const Env = __dirname + '\\..\\src\\core\\config\\env.json'
-let bkp, prod = ''
+const envDir = __dirname + '/../src/core/config/env.json'
+const enviroment = require(envDir)
 
-function readEnv () {
-  return new Promise(function (resolve, reject) {
-    try {
-      fs.readFile(Env, (err, content) => {
-        resolve(JSON.parse(content))
-      })
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
+let bkp   = Object.assign({}, enviroment)
+let prod  = enviroment
 
-readEnv()
-.then(res => {
-  bkp, prod = Object.assign({}, res)
-  prod.enviroment = 'prod'
-})
-.catch( err => console.log("Houve um erro ao compilar: " + err) )
+prod.env = 'prod'
 
-function build () {
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(Env, JSON.stringify(prod, null, 2), function (err) {
-      if (err) {
-        return console.log("Erro ao salvar aquivo.")
+// Altera o ambiente interno da aplicação para produção
+fs.writeFile(envDir, JSON.stringify(prod, null, 2), function (err) {
+  // Caso haja algum erro ao escrever o arquivo
+  if (err){
+    console.log("Erro ao compilar: não foi possível alterar o ambiente.\n")
+    console.log(chalk.red(err))
+    return;
+  }
+
+  // Informa ao usuário que está iniciando a compilar
+  const spinner = ora('Compilando...')
+  spinner.start()
+
+  // Processo de compilação
+  rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err =>  {
+    // Caso haja algum erro
+    if (err) throw err
+
+    webpack(webpackConfig, (err, stats) =>  {
+      // Para de girar o Spinner
+      spinner.stop()
+
+      // Caso haja algum erro ao compilar o Webpack
+      if (err) throw err
+
+
+      process.stdout.write(stats.toString({
+        colors:true,
+        modules:false,
+        children:false, // Caso esteja usando o ts-loader, setar isso como true irá fazer com que os erros do TypeScript sejam exibidos durante a compilação
+        chunks:false,
+        chunkModules:false
+      }) + '\n\n')
+
+      // Caso haja algum erro nos stats webpack
+      if (stats.hasErrors()) {
+        console.log(chalk.red('Erro durante a compilação.\n'))
+        process.exit(1)
       }
-      process.env.NODE_ENV = 'production'
-  
-      const spinner = ora('Buildando para produção...')
-      spinner.start()
-      
-      rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-        if (err) throw err
-        webpack(webpackConfig, (err, stats) => {
-          spinner.stop()
-          if (err) throw err
-          process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-            chunks: false,
-            chunkModules: false
-          }) + '\n\n')
-      
-          if (stats.hasErrors()) {
-            console.log(chalk.red('  Build failed with errors.\n'))
-            process.exit(1)
-          }
-      
-          fs.writeFile(Env, JSON.stringify(bkp, null, 2), function (err) {
-            console.log(chalk.cyan('  Build finalizado com sucesso.\n'))
-            console.log(chalk.yellow(
-              '  Copie a pasta.\n' +
-              '  Opening index.html over file:// won\'t work.\n'
-            ))
-          })
-        })
+
+      // Após compilado, retorna ao enviroment anterior
+      fs.writeFile(envDir, JSON.stringify(bkp, null, 2), function (err) {
+        // Caso haja algum erro ao escrever o arquivo
+        if (err){
+          console.log('Compilado, porém não foi possível reverter para o ambiente anterior.\n')
+          console.log('Rode o compando lummi --env={seu ambiente} ou altere manualmente')
+          console.log(chalk.red(err))
+          return
+        }
+
+        console.log(chalk.cyan('Compilado com sucesso.\n'))
+        console.log(chalk.yellow(
+        'Copie a pasta \'dist\' para seu servidor HTTP .\n' +
+        'Abrir o aquivo index.html sob o protocolo file:// não irá funcionar.\n'))
       })
     })
   })
-}
+})
