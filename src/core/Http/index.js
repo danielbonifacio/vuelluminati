d616be25d60876ade05b1b15c144b0411995e815
Daniel Bonifacio <danielbonifacio@outlook.com>
2018-08-10 15:42:01 -0300

    Correção do Http

--- src/core/Http/index.js
+++ src/core/Http/index.js
@@ -1,18 +1,5 @@
 import axios from './http.config';
 
-const Http = {
-  get: url => new Promise((resolve, reject) => {
-    axios
-      .get(url)
-      .then(res => resolve(res))
-      .catch(err => reject(err));
-  }),
-  post: (url, post) => new Promise((resolve, reject) => {
-    axios
-      .post(url, post)
-      .then(res => resolve(res))
-      .catch(err => reject(err));
-  }),
-};
+const Http = axios;
 
 export default Http;
